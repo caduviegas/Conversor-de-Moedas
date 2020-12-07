8ac147c6b42d0d364af42fbfaacccdee19345957
Eduardo Viegas <engenedu@hotmail.com>
2020-12-07 16:59:43 -0300

    criando o xml

--- app/src/main/java/br/com/innaval/conversordemoedas/MainActivity.kt
+++ app/src/main/java/br/com/innaval/conversordemoedas/MainActivity.kt
@@ -2,10 +2,19 @@ package br.com.innaval.conversordemoedas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val etValor =findViewById<EditText>(R.id.etValor)
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+        val btConverter = findViewById<Button>(R.id.btConverter)
+        btConverter.setOnClickListener {
+
+        }
     }
 }
